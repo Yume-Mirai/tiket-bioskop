833c94ca7cf8b1472b097342e20c5a1f200406f2
Putri Ayu <putriayusahara35@gmail.com>
2025-03-23 14:23:49 +0700

    first commit

---
+++ src/main/java/com/uasjava/tiketbioskop/TiketbioskopApplication.java
@@ -0,0 +1,13 @@
+package com.uasjava.tiketbioskop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiketbioskopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiketbioskopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uasjava/tiketbioskop/TiketbioskopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uasjava.tiketbioskop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiketbioskopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
